a40edd1f23fcacdbe31506e1c1da90ffc71a21af
joaocarrato <joaocarrato18@gmail.com>
2025-03-09 22:47:33 -0300

    Add pre-commit hooks for linting and TypeScript checks; configure VSCode settings and define color palette

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: '.',
+        alias: {
+          '@api': './src/api',
+          '@components': './src/components',
+          '@domain': './src/domain',
+          '@hooks': './src/hooks',
+          '@routes': './src/routes',
+          '@screens': './src/screens',
+          '@services': './src/services',
+          '@theme': './src/theme',
+        },
+      },
+    ],
+  ],
 };
---
+++ src/theme/theme.ts
@@ -0,0 +1,7 @@
+export const palette = {
+  auburn: '#A62639',
+  amaranth: '#DB324D',
+  wenge: '#56494E',
+  taupeGray: '#A29C9B',
+  chocolateCosmos: '#511c29',
+};
