c35c331f0024a0af8a27cebdab6e18b13dcade80
joaocarrato <joaocarrato18@gmail.com>
2025-03-09 22:58:40 -0300

    Refactor App component and implement custom Text component with theming support

---
+++ src/components/Text/Text.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import {TextStyle} from 'react-native';
+
+import {createText} from '@shopify/restyle';
+
+import {Theme} from '@theme';
+
+const SRText = createText<Theme>();
+type SRTextProps = React.ComponentProps<typeof SRText>;
+
+export interface TextProps extends SRTextProps {
+  preset?: TextVariants;
+  semiBold?: boolean;
+  bold?: boolean;
+}
+
+export function Text({
+  children,
+  preset = 'paragraphMedium',
+  semiBold,
+  bold,
+  style,
+  ...srTextProps
+}: TextProps) {
+  const fontFamily = getFontFamily(preset, semiBold, bold);
+  return (
+    <SRText style={[$fontSizes[preset], {fontFamily}, style]} {...srTextProps}>
+      {children}
+    </SRText>
+  );
+}
+
+function getFontFamily(
+  preset: TextVariants,
+  semiBold?: boolean,
+  bold?: boolean,
+) {
+  if (
+    preset === 'headingLarge' ||
+    preset === 'headingMedium' ||
+    preset === 'headingSmall'
+  ) {
+    return $fontFamily.bold;
+  }
+
+  switch (true) {
+    case bold:
+      return $fontFamily.bold;
+    case semiBold:
+      return $fontFamily.semiBold;
+    default:
+      return $fontFamily.regular;
+  }
+}
+
+// Type of text variants
+type TextVariants =
+  | 'headingLarge'
+  | 'headingMedium'
+  | 'headingSmall'
+  | 'paragraphLarge'
+  | 'paragraphMedium'
+  | 'paragraphSmall'
+  | 'paragraphCaption'
+  | 'paragraphCaptionSmall';
+
+// Font size and line height
+export const $fontSizes: Record<TextVariants, TextStyle> = {
+  headingLarge: {fontSize: 32, lineHeight: 38.4},
+  headingMedium: {fontSize: 22, lineHeight: 26.4},
+  headingSmall: {fontSize: 18, lineHeight: 23.4},
+
+  paragraphLarge: {fontSize: 18, lineHeight: 25.2},
+  paragraphMedium: {fontSize: 16, lineHeight: 22.4},
+  paragraphSmall: {fontSize: 14, lineHeight: 19.6},
+
+  paragraphCaption: {fontSize: 12, lineHeight: 16.8},
+  paragraphCaptionSmall: {fontSize: 10, lineHeight: 14},
+};
+
+const $fontFamily = {
+  bold: 'Poppins-Bold',
+  regular: 'Poppins-Regular',
+  semiBold: 'Poppins-SemiBold',
+};
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,7 +1,48 @@
-export const palette = {
+import {createTheme} from '@shopify/restyle';
+
+const palette = {
   auburn: '#A62639',
   amaranth: '#DB324D',
   wenge: '#56494E',
   taupeGray: '#A29C9B',
   chocolateCosmos: '#511c29',
+
+  white: '#ffffff',
+  gray: '#7C7375',
 };
+
+export const theme = createTheme({
+  colors: {
+    ...palette,
+
+    background: palette.taupeGray,
+    backgroundContrast: palette.gray,
+
+    primary: palette.chocolateCosmos,
+    primaryContrast: palette.wenge,
+  },
+  spacing: {
+    s4: 4,
+    s8: 8,
+    s12: 12,
+    s16: 16,
+    s20: 20,
+    s24: 24,
+    s28: 28,
+    s32: 32,
+    s36: 36,
+    s40: 40,
+    s48: 48,
+  },
+  borderRadii: {
+    s8: 8,
+    s12: 12,
+    s16: 16,
+  },
+  textVariants: {
+    defaults: {},
+  },
+});
+
+export type Theme = typeof theme;
+export type ThemeColors = keyof Theme['colors'];
